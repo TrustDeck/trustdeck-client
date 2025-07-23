9af840a6fc35bfec9d64e8a06b898bf523172f0d
Armin Müller <armin.mueller@bih-charite.de>
2025-07-23 15:03:02 +0200

    Removed unused method.

--- src/main/java/org/trustdeck/client/util/TrustDeckRequestUtil.java
+++ src/main/java/org/trustdeck/client/util/TrustDeckRequestUtil.java
@@ -20,7 +20,6 @@ package org.trustdeck.client.util;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.trustdeck.client.model.Domain;
 import org.trustdeck.client.service.TrustDeckTokenService;
 
 /**
@@ -28,7 +27,7 @@ import org.trustdeck.client.service.TrustDeckTokenService;
  * 
  * @author Chethan Nagaraj, Armin Müller
  */
-public class TrustDeckClientUtil {
+public class TrustDeckRequestUtil {
 	
 	/** Enables access to the token handling service. */
 	private TrustDeckTokenService tokenService;
@@ -38,7 +37,7 @@ public class TrustDeckClientUtil {
 	 * 
 	 * @param tokenService an instance of the token handling service
 	 */
-	public TrustDeckClientUtil(TrustDeckTokenService tokenService) {
+	public TrustDeckRequestUtil(TrustDeckTokenService tokenService) {
 		this.tokenService = tokenService;
 	}
 	
@@ -74,89 +73,5 @@ public class TrustDeckClientUtil {
         
         // Build request entity
         return new HttpEntity<>(body, headers);
-    }
-    
-    /**
-     * Helper method that extracts an attribute out of a domain.
-     * 
-     * @param domain the domain containing the attribute of interest
-     * @param attributeName the attribute of interest
-     * @return the attribute as a String
-     */
-    public String extractAttributeOutOfDomain(Domain domain, String attributeName) {
-    	// Get required attribute
-    	switch (attributeName.trim().toLowerCase()) {
-		case "id": {
-			return domain.getId().toString();
-		} case "name": {
-			return domain.getName();
-		} case "prefix": {
-			return domain.getPrefix();
-		} case "validfrom": {
-			return domain.getValidFrom().toString();
-		} case "validfrominherited": {
-			return domain.getValidFromInherited().toString();
-		} case "validto": {
-			return domain.getValidTo().toString();
-		} case "validtoinherited": {
-			return domain.getValidToInherited().toString();
-		} case "enforcestartdatevalidity": {
-			return domain.getEnforceStartDateValidity().toString();
-		} case "enforcestartdatevalidityinherited": {
-			return domain.getEnforceStartDateValidityInherited().toString();
-		} case "enforceenddatevalidity": {
-			return domain.getEnforceEndDateValidity().toString();
-		} case "enforceenddatevalidityinherited": {
-			return domain.getEnforceEndDateValidityInherited().toString();
-		} case "algorithm": {
-			return domain.getAlgorithm();
-		} case "algorithminherited": {
-			return domain.getAlgorithmInherited().toString();
-		} case "alphabet": {
-			return domain.getAlphabet();
-		} case "alphabetinherited": {
-			return domain.getAlphabetInherited().toString();
-		} case "randomalgorithmdesiredsize": {
-			return domain.getRandomAlgorithmDesiredSize().toString();
-		} case "randomalgorithmdesiredsizeinherited": {
-			return domain.getRandomAlgorithmDesiredSizeInherited().toString();
-		} case "randomalgorithmdesiredsuccessprobability": {
-			return domain.getRandomAlgorithmDesiredSuccessProbability().toString();
-		} case "randomalgorithmdesiredsuccessprobabilityinherited": {
-			return domain.getRandomAlgorithmDesiredSuccessProbabilityInherited().toString();
-		} case "multiplepsnallowed": {
-			return domain.getMultiplePsnAllowed().toString();
-		} case "multiplepsnallowedinherited": {
-			return domain.getMultiplePsnAllowedInherited().toString();
-		} case "consecutivevaluecounter": {
-			return domain.getConsecutiveValueCounter().toString();
-		} case "pseudonymlength": {
-			return domain.getPseudonymLength().toString();
-		} case "pseudonymlengthinherited": {
-			return domain.getPseudonymLengthInherited().toString();
-		} case "paddingcharacter": {
-			return domain.getPaddingCharacter().toString();
-		} case "paddingcharacterinherited": {
-			return domain.getPaddingCharacterInherited().toString();
-		} case "addcheckdigit": {
-			return domain.getAddCheckDigit().toString();
-		} case "addcheckdigitinherited": {
-			return domain.getAddCheckDigitInherited().toString();
-		} case "lengthincludescheckdigit": {
-			return domain.getLengthIncludesCheckDigit().toString();
-		} case "lengthincludescheckdigitinherited": {
-			return domain.getLengthIncludesCheckDigitInherited().toString();
-		} case "salt": {
-			return domain.getSalt();
-		} case "saltlength": {
-			return domain.getSaltLength().toString();
-		} case "description": {
-			return domain.getDescription();
-		} case "superdomainid": {
-			return domain.getSuperDomainID().toString();
-		} default:
-			// The requested attribute was not found.
-			return null;
-		}
     }
 }
